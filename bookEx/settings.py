37cd0ef437267f26ef7094be03d545394a45dcac
Cristian <34379751+CrisM6285@users.noreply.github.com>
2021-05-12 20:08:24 -0700

    P to p

--- bookEx/settings.py
+++ bookEx/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '$tix#l0m9bgyd3t!4_+4x+xysyog*vzynpv!i!mtu#(0sa$2$m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
